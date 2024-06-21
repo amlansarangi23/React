5905249dbb65d5fea08f6f225eb750281f46eb49
Amlan Sarangi <amlan.sarangi23@gmail.com>
2024-06-21 19:48:47 +0530

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
-    // object destructuring in props
+const MovieCard = ({ movie }) => {
+  const { imdbID, Year, Poster, Title, Type, Plot } = movie;
+
   return (
-    <div className="movie" key={imdbID}>
-      <div>
-        <p>{Year}</p>
+    <Link to={`/movie/${imdbID}`} state={{ movie }} className="movie-card">
+      <div className="movie">
+        <div>
+          <p>{Year}</p>
+        </div>
+        <div>
+          <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+        </div>
+        <div>
+          <span>{Type}</span>
+          <h3>{Title}</h3>
+          <p>{Plot}</p> {/* Display the plot here */}
+        </div>
       </div>
-
-      <div>
-        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />  
-      </div>
-      {/* if no image found */}
-
-      <div>
-        <span>{Type}</span>
-        <h3>{Title}</h3>
-      </div>
-    </div>
+    </Link>
   );
 }
 
