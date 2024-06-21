1f7aecdc3acde984c09e5dab1f52523c0ba17457
Amlan Sarangi <amlan.sarangi23@gmail.com>
2024-06-21 19:48:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,115 +1,59 @@
-// import React from "react";
-// //eafdeb1c
-// import { useEffect } from "react";
-// import './App.css';
-// import SearchIcon from './search.svg';
-// const API_URL ='http://www.omdbapi.com/?i=tt3896198&apikey=eafdeb1c';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MovieCard from './MovieCard';
+import MovieDetails from './MovieDetails';
+import SearchBar from './SearchBar';
+import './App.css';
 
-
-// const movie1={
-//         "Title": "Spider-Man Title Reveal",
-//         "Year": "2021",
-//         "imdbID": "tt14122734",
-//         "Type": "movie",
-//         "Poster": "https://m.media-amazon.com/images/M/MV5BNjRjMmQ2NDQtNmI5NC00N2EwLTkwYWQtOTM2OGZjMmI5YmRjXkEyXkFqcGdeQXVyMTI0NTA1MDI3._V1_SX300.jpg"
-// }
-
-// const App=() =>{
-// const searchMovies = async(title)=>{
-//    const response = await fetch(`${API_URL}&s={title}`);
-//    const data = await response.json();
-
-//    console.log(data.Search);
-// }
-//    useEffect(()=>{
-//    searchMovies('Spiderman');
-// },[]);
-
-//     return(
-//         <div className="app">
-//             <h1>MovieLand</h1>
-
-//             <div className="search">
-//                 <input placeholder="Search for movies" value="Superman"
-//                  onChange={()=>{}} 
-//                  />
-//                 <img src={SearchIcon}
-//                 alt="search"
-//                 onClick={()=>{}}
-//                 >
-//                 </img>
-               
-//             </div>
-//             <div className="container">
-//                 <div className="movie">
-//                     <div>
-//                         <p>{movie1.Title}</p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-        
-//     );
-// }
-
-// export default App;
-
-
-
-import React, { useState, useEffect } from "react";
-
-import MovieCard from "./MovieCard";
-import SearchIcon from "./search.svg";
-import "./App.css";
-
-const API_URL = "https://www.omdbapi.com??i=tt3896198&apikey=b6003d8a";
+const API_URL = 'https://www.omdbapi.com?i=tt3896198&apikey=b6003d8a';
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies('Batman');
   }, []);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
+    console.log("OMDB API Response:", data);
+
     setMovies(data.Search);
   };
 
   return (
-    <div className="app">
-      <h1>MOVIE MAGIC</h1>
-
-      <div className="search">
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
+    <Router>
+      <div className="app">
+        <h1>WAREHOUSE</h1>
+        <SearchBar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          searchMovies={searchMovies}
         />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              movies?.length > 0 ? (
+                <div className="container">
+                  {movies.map((movie) => (
+                    <MovieCard key={movie.imdbID} movie={movie} />
+                  ))}
+                </div>
+              ) : (
+                <div className="empty">
+                  <h2>No movies found</h2>
+                </div>
+              )
+            }
+          />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
       </div>
-
-
-      {/* Ternary Operator */}
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
-      )}
-    </div>
+    </Router>
   );
 };
 
